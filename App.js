bc42a7a37e74a3e2bb03a26f81eb8f9ff7db7667
Cosmos <Alfredchrisayo@gmail.com>
2023-06-26 20:04:37 -1200

    implemeted message read status from the admin to the departments

--- App.js
+++ App.js
@@ -1,97 +1,105 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const { config } = require("dotenv");
+const cookie = require("cookie-parser");
+const {
+  createDepartment,
+  sendMessage,
+  createMemo,
+  User,
+  getMemos,
+  readMessage,
+} = require("./Logic.js");
+config();
+const port = process.env.PORT;
 
-const { config } = require('dotenv')
-const cookie = require('cookie-parser')
-const { createDepartment, sendMessage, createMemo, User, getMemos, readMessage } = require('./Logic.js');
-config()
-const port = process.env.PORT
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
+const jwt = require("jsonwebtoken");
 
-const jwt = require('jsonwebtoken');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.text())
 app.use(express.json());
-app.use(cors())
-app.use(cookie())
-
-
+app.use(cors());
+app.use(cookie());
 
 async function Auth(req, res, next) {
-    const token = req.cookies.token;
-    if (!token) {
-        res.send({ response: 'unauthorized user, sign in to your account' })
-    }
-    else {
-        const data = jwt.verify(token, process.env.SECRET_KEY);
-        req.user = data
-        next()
-    }
+  const token = req.cookies.token;
+  if (!token) {
+    res.send({ response: "unauthorized user, sign in to your account" });
+  } else {
+    const data = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = data;
+    next();
+  }
 }
 
-
-
-
-app.post('/account/login', async (req, res) => {
-    const { id, password } = req.body
-    const user = await User(id);
-    if (user) {
-        if (user.password === password) {
-            const token = jwt.sign(user, process.env.SECRET_KEY, { expiresIn: '1d' });
-            res.cookie('token', token, { maxAge: '30000', httpOnly: true, path: '/' });
-            res.redirect('/memos')
-        }
-        else {
-            res.send({ response: 'invalid password' })
-            return;
-        }
+app.post("/account/login", async (req, res) => {
+  const { name, password } = req.body;
+  const user = await User(name);
+  if (user) {
+    const matchPass = await bcrypt.compare(password, user.password);
+    if (matchPass) {
+      const token = jwt.sign(user, process.env.SECRET_KEY, { expiresIn: "1d" });
+      res.cookie("token", token, {
+        maxAge: "30000",
+        httpOnly: true,
+        path: "/",
+      });
+      res.redirect("/memos");
+    
+    } else {
+      res.send({ response: "invalid password" });
+      return;
     }
-    else {
-        res.send({ response: 'User not found' });
-        return;
+  } else {
+    res.send({ response: "User not found" });
+    return;
+  }
+});
+
+app.post("/private/message", async (req, res) => {
+  const { id, message } = req.body;
+  const data = await sendMessage(id, message);
+  res.send(data);
+});
+
+app.get("/memos", Auth, async (req, res) => {
+  const memos = await getMemos();
+  res.status(200).send(memos);
+});
+
+app.post("/memo", async (req, res) => {
+  const data = await createMemo(req.body);
+  res.status(200).send(data);
+});
+
+app.post("/account/signup", async (req, res) => {
+  const { name, password } = req.body;
+  try {
+    const existUser = await User(name);
+    if (existUser) {
+      res.send({ response: "Account already exist" });
+    } else {
+      const newAccount = await createDepartment(name, password);
+      res.send({ resposne: "Success, Welcome on board", data: newAccount });
     }
-
-});
-
-app.post('/private/message', async (req, res) => {
-    const { id, message } = req.body;
-    const data = await sendMessage(id, message);
-    res.send(data);
-});
-
-
-app.get('/memos', Auth, async (req, res) => {
-    const memos = await getMemos();
-    res.status(200).send(memos)
-});
-
-app.post('/memo', async (req, res) => {
-    const data = await createMemo(req.body);
-    res.status(200).send(data)
-
+  } catch (error) {
+    res.send("Registration not successful, try again");
+  }
 });
 
 
 
-
-app.post('/account/signup', async (req, res) => {
-    const { name, password } = req.body;
-    const userAccount = await createDepartment(name, password)
-    res.send(userAccount);
+app.patch("/private/readmessage", Auth, async (req, res) => {
+  const { id, messageId } = req.body;
+  const status = await readMessage(id, messageId);
+  res.send(status);
 });
-
-app.patch('/private/readmessage', Auth, async (req, res) => {
-    const { id, messageId } = req.body
-    const status = await readMessage(id, messageId);
-    res.send(status);
-
-});
-
-
 
 app.listen(port, function () {
-    console.log(`Server running on ${port}`)
-})
+  console.log(`Server running on ${port}`);
+});
--- Logic.js
+++ Logic.js
@@ -1,119 +1,128 @@
 // import { MongoClient } from 'mongodb'
 // import { config } from "dotenv";
-const { MongoClient } = require('mongodb');
-const { config } = require('dotenv')
+const { MongoClient } = require("mongodb");
+const { config } = require("dotenv");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-config()
-const client = new MongoClient(process.env.MONGO_URI)
-
+config();
+const client = new MongoClient(process.env.MONGO_URI);
 
 const connection = async () => {
-    const connect = await client.connect();
-    if (connect) {
-        return 'connected to the database';
-    }
-    else {
-        return 'connection not established'
-    }
-}
+  const connect = await client.connect();
+  if (connect) {
+    return "connected to the database";
+  } else {
+    return "connection not established";
+  }
+};
 
-const collection = client.db('Oyscatech').collection('administration');
-const memorandum = client.db('Oyscatech').collection('memo');
-const likes = client.db('Oyscatech').collection('likes');
+const collection = client.db("Oyscatech").collection("administration");
+const memorandum = client.db("Oyscatech").collection("memo");
+const messages = client.db("Oyscatech").collection("messages");
 
+const User = async (name) => {
+  const user = await collection.findOne({ name: name });
+  return user;
+};
 
-const User = async (id) => {
-    const user = await collection.findOne({ id: id })
-    return user;
-}
-
-
+//createDepartmemnt
 
 const createDepartment = async (name, password) => {
-    const departmentId = Math.floor(Math.random() * 98765 + 1234);
-    const accountID = await collection.insertOne({
-        id: departmentId,
-        name: name,
-        password: password,
-        messages: []
-    });
-    return User(departmentId)
-}
-
-const sendMessage = async (id, message) => {
-    const messageId = Math.floor(Math.random() * 862 + 123 + 2023)
-    const sendMessage = await collection.updateOne({ id: id },
-        {
-            $push: {
-                messages: {
-                    id: messageId,
-                    date: new Date().toLocaleDateString(),
-                    time: new Date().toLocaleTimeString(),
-                    message: message,
-                    readStatus: false
-                }
-            }
-        }
-    )
-
-    if (sendMessage) {
-        return "message sent succesfully "
-    }
-    else { return "message not sent try again" }
+  const departmentId = Math.floor(Math.random() * 98765 + 1234);
+  const encryptedPassword = await bcrypt.hash(password, saltRounds);
+  await collection.insertOne({
+    id: departmentId,
+    name: name,
+    password: encryptedPassword,
+    messages: [],
+    outbox: [],
+  });
+  return User(name);
 };
 
+//sendMessage func
+const sendMessage = async (id, message) => {
+  const messageId = Math.floor(Math.random() * 862 + 123 + 2023);
 
+  await messages.insertOne({
+    id: messageId,
+    date: new Date().toLocaleDateString(),
+    time: new Date().toLocaleTimeString(),
+    message: message,
+    readStatus: false,
+  });
 
+  const sendMessage = await collection.updateOne(
+    { id: id },
+    {
+      $push: {
+        messages: {
+          id: messageId,
+          date: new Date().toLocaleDateString(),
+          time: new Date().toLocaleTimeString(),
+          message: message,
+          readStatus: false,
+        },
+      },
+    }
+  );
 
+  if (sendMessage) {
+    return "message succesfully sent~~";
+  } else {
+    return "message not sent try again";
+  }
+};
 
-async function createMemo({ heading: heading, body: body, cc: cc }) {
-    const memoId = Math.floor(Math.random() * 862 + 123 * 2023);
-    await memorandum.insertOne({
-        id: memoId,
-        heading: heading,
-        date: new Date().toLocaleDateString(), time: new Date().toLocaleTimeString(),
-        body: body,
-        cc: cc
-    });
-    return getMemos()
+async function createMemo({
+  cc: cc,
+  from: from,
+  heading: heading,
+  body: body,
+  to: to,
+}) {
+  const memoId = Math.floor(Math.random() * 862 + 123 * 2023);
+  await memorandum.insertOne({
+    id: memoId,
+    heading: heading,
+    from: from,
+    to: to,
+    date: new Date().toLocaleDateString(),
+    time: new Date().toLocaleTimeString(),
+    body: body,
+    cc: cc,
+  });
+  return getMemos();
 }
-
 
 async function getMemos() {
-    const memos = memorandum.find({}).toArray();
-    return memos
+  const memos = memorandum.find({}).toArray();
+  return memos;
 }
-
 
 async function readMessage(id, messageId) {
-    const readStatus = await collection.updateOne({ id: id, "messages.id": messageId },
-        {
-            $set: { "messages.$.readStatus": true }
-        });
-    return readStatus;
-};
+  await messages.updateOne({ id: messageId }, { $set: { readStatus: true } });
 
-
+  const readStatus = await collection.updateOne(
+    { id: id, "messages.id": messageId },
+    {
+      $set: { "messages.$.readStatus": true },
+    }
+  );
+  return readStatus;
+}
 
 async function getMemo(id) {
-    const memo = await memorandum.findOne({ id: id });
-    return memo;
+  const memo = await memorandum.findOne({ id: id });
+  return memo;
 }
 
-
-
-async function likes(memo, user) {
-    const memo = await getMemo(id);
-    const user = await User(user)
-    if (memo && user) {
-
-    }
-}
-
-
-const memo = await getMemo(248929);
-console.log(memo)
-
-module.exports = { createDepartment, createMemo, User, sendMessage, getMemos, readMessage }
-
-
+module.exports = {
+  createDepartment,
+  createMemo,
+  User,
+  sendMessage,
+  getMemos,
+  readMessage,
+};
